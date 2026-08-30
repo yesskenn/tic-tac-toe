262e26c703e4f06055fc2ec2a4d60d20b7a00450
Yessi Kenney <yessikenney@gmail.com>
2026-08-30 09:29:48 -0700

    add spot mapping, target cell, remove empty functions, fix playturn

--- script.js
+++ script.js
@@ -1,108 +1,134 @@
-
-function play(){
-    function board(){
-
+function gameBoard(){
     const board = []
-    const cell = " ";
     const rows = 3;
-    const columns = 3;
-    const abc = ["A", "B", "C"];
+    const columns = 3;   
+    
+ 
+   //drawing the mf'ing board
 
-    let column = [];
-    for (let i = 0; i < rows; i++) {
-    let row = [];
-        if (i/i===0) {
-            board.push(row);
+    for (let i = 0; i < rows; i++){
+        board[i] = [];
+        for (let j=0; j < columns; j++){
+        board[i].push(cell());
         }
-        for (let j = 0; j <= columns; j++) {
-            let column = [];
-            if(i/j === 0){
-                board.push(column);  
-                }       
-            }  
-            console.log(board);   
-        }
+    }   
 
+    //inspo /help from the connect4 guy
+    const getBoard = () => board;
+    
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+    console.table(boardWithCellValues);
+  };
+
+function spots(){
+const spot = {
+
+     1: board[0][0],
+     2: board[0][1],
+     3: board[0][2],
+     4: board[1][0],
+     5: board[1][1],
+     6: board[1][2],
+     7: board[2][0],
+     8: board[2][1],
+     9: board[2][2]
+}
+return spot;
+}
+
+const currentSpots = spots();
+  // game.playTurn(1);
+//NEXT IS TO LOOK THROUGH AVAILABLE TIC TAC TOE CELLS AND FIND AN EMPTY ONE BUT DROP THE TOKEN WHERE THE PLAYER SAYS THEY WANT IT 
+const dropSpot = (spot, player) => {
+    const targetCell = currentSpots[spot];
+    
+        targetCell.addToken(player);
+    
+    
+    //if the player chooses a spot number, add the player token to the spot on the board
+}
+  return { getBoard, dropSpot, currentSpots, printBoard};
+
+}
+
+function cell(){
+    let value = 0;
+//change the value of the cell with player's token
+    const addToken = (player) => {
+        value = player;
+    };
+//retrieves cell value through closure (from building a house from the inside out)
+    const getValue = () => value;
+
+    return {
+        addToken,
+        getValue,
+    };
+    }
+  
+
+
+
+function gameController(
+    playerXname = "PlayerX",
+    playerOname = "PlayerO"
+){
+
+    const board = gameBoard();
+
+    const players = [
+        {
+            name: playerXname,
+            token: "X",
+        },
+        {
+            name: playerOname,
+            token: "O",
+        },
+    ]
+    
+    //take turns between players X and O
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn`)
     };
 
+    const playTurn = (spot) => {
+        console.log( `{getActivePlayer().name}'s token into spot ${spot}`)
+        board.dropSpot(spot, getActivePlayer().token);
 
-    function chooseMarker(){
-        playerX = "PlayerX",
-        playerO = "PlayerO"
-   
-
-        const players = [
-            {name: playerX,
-                token: "X",
-
-            },
-            {name: playerO,
-                token: "O",
-            }
-        ];
+        switchPlayerTurn();
+        printNewRound();
     }
-    chooseMarker();
 
-    console.log("X or O?")
-     board();
+    printNewRound();
+
+    return {
+        playTurn,
+        getActivePlayer,
+    };
 }
 
-
-function Cell(){
-    let value = 0; 
-
-    //accept x or o
-
-    const addToken = (player) => value;
-        return {
-            addToken,
-            getValue,
-        };
-
-  }
-  
-play();
-    /* 
-const getBoard = () => board;
+const game = gameController();
 
 
+//write out your spot like "game.playTurn(1)"
     
-const addMarker = (column, player) => {
-    const availableCells = board 
-        .filter((row) => [column].getValue() === " ")
-        .map((row) => row[column]);
-
-
-        const lowRow = availableCells.length-1;
-        board[lowRow][column].addMarker(player);
-};
-
-
-const renderBoard = () => {
-    const boardWithCellValues = board.map((row) =>
-        row.map((cell) => cell.getValue())
-);
-console.log(boardWithCellValues);
-}
-
-
-return { getBoard, addMarker, renderBoard};
-
-
-
-
-   function game(
-    
-
-
-    let activePlayer = players[0]
-   }
-   }
-        //select where the x or o is going to go on the board/grid
-        //update the board
-        //return the result
-board();
-
-
-*/
-
+    // pick a spot on the table, between 1 and 9
+    // replace that spot with X or O depending on active player
+    // refresh the table // check for winner
+            // check winning combinations for X and winning combinations for O 
+            // if winner, then declare winner
+            // else continue
+    // and switch active player
